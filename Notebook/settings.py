6748cc28a316ca28cc43e6acb3029ad70cfce323
Miquel Àngel Rigo <marigov@mac.com>
2017-11-03 12:13:50 +0000

    .

--- Notebook/settings.py
+++ Notebook/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'Notebook.wsgi.application'
 
 
 # Databases
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.11/ref/settings/#database
 
 DEPLOY = False
 
